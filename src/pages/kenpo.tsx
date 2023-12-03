80330d5cbc992d4ca99007e1120ecb06d8a1f2f4
Kato Hisashi <wo8ixp8i@outlook.jp>
2023-12-03 13:19:47 +0900

    pdfの内容をテキストレイヤーで表示して対象文字にラインを引いた

--- src/pages/kenpo.tsx
+++ src/pages/kenpo.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as pdfjsLib from "pdfjs-dist";
-import { type PDFDocumentProxy, type PDFPageProxy } from "pdfjs-dist/types/src/display/api";
-import { type RenderTask } from "pdfjs-dist/types/src/display/api";
+import {
+  type PDFDocumentProxy,
+  type PDFPageProxy,
+} from "pdfjs-dist/types/src/display/api";
+import {
+  type RenderTask,
+  type TextItem,
+} from "pdfjs-dist/types/src/display/api";
+import { renderTextLayer } from "pdfjs-dist";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdf.worker.mjs";
 
 const Kenpo: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [pdfText, setPdfText] = useState("");
   const pdfPath = "/pdf/日本国憲法.pdf";
   let renderTask: RenderTask | null = null;
 
@@ -18,12 +26,36 @@ const Kenpo: React.FC = () => {
       const viewport = page.getViewport({ scale });
       const outputScale = window.devicePixelRatio || 1;
 
+      const textLayerDiv = document.getElementById("textLayer");
+      const textContent = await page.getTextContent();
+
       const canvas = canvasRef.current;
       if (canvas && renderTask) {
         renderTask.cancel();
         renderTask = null;
       }
 
+      // テキストレイヤーのレンダリング
+      // TODO:各ノードは現状1文字など区切りがバラバラなため文字列に対してアンダーラインが引けない
+      if (textLayerDiv) {
+        const textLayerRenderTask = renderTextLayer({
+          textContentSource: textContent, // テキストコンテンツの指定
+          container: textLayerDiv, // コンテナ要素
+          viewport: viewport, // ビューポート
+        });
+        await textLayerRenderTask.promise;
+        const textLayerNodes = await document.querySelectorAll("#textLayer");
+        textLayerNodes.forEach((node) => {
+          console.log(node);
+          node.querySelectorAll("span").forEach((span) => {
+            if (span.textContent?.includes("国")) {
+              span.classList.add("bg-yellow-200");
+            }
+          });
+        });
+      }
+
+      // キャンバスへの描画
       if (canvas) {
         const context = canvas.getContext("2d");
         if (context) {
@@ -32,7 +64,8 @@ const Kenpo: React.FC = () => {
           canvas.style.width = `${Math.floor(viewport.width)}px`;
           canvas.style.height = `${Math.floor(viewport.height)}px`;
 
-          const transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
+          const transform =
+            outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
 
           const renderContext = {
             canvasContext: context,
@@ -43,19 +76,35 @@ const Kenpo: React.FC = () => {
           try {
             renderTask = page.render(renderContext);
             await renderTask.promise;
-          }catch(e){
+
+            // テキストコンテンツの取得
+            await getPdfText();
+          } catch (e) {
             console.log(e);
           }
         }
+      }
+
+      async function getPdfText() {
+        const textContent = await page.getTextContent();
+        const textItems = textContent.items
+          .filter((item): item is TextItem => "str" in item)
+          .map((item) => item.str)
+          .join(" ");
+        setPdfText(textItems);
+        console.log(textItems);
       }
     });
   }, []);
 
   return (
-    <>
-      <div>日本国憲法</div>
-      <canvas ref={canvasRef}></canvas>
-    </>
+    <div id="pdf-container">
+      {/* <canvas ref={canvasRef}></canvas> */}
+      <div
+        id="textLayer"
+        style={{ position: "absolute", left: 0, top: 0 }}
+      ></div>
+    </div>
   );
 };
 
